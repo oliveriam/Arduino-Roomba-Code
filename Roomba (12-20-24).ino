fcd50e11c6b8477540e0b4cfdb8942d355e56628
Oliver Maranda <148398363+oliveriam@users.noreply.github.com>
2025-01-19 17:36:09 -0500

    Roomba (12-20-24).ino

--- Roomba (12-20-24).ino
+++ Roomba (12-20-24).ino
@@ -32,27 +32,21 @@ char* convert_int16_to_str(int16_t i) {
 
 void initializeSensor()
 {
-  //
-  // Perfrom full reset as per MPU-6000/MPU-6050 Register Map and Descriptions, Section 4.28, pages 40 to 41.
-  //
-
-  // performing full device reset, disables temperature sensor, disables SLEEP mode
-  Wire.beginTransmission(0x68);  // Device address.
-  Wire.write(0x6B);              // PWR_MGMT_1 register.
-  Wire.write(0b10001000);        // DEVICE_RESET, TEMP_DIS.
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x6B);           
+  Wire.write(0b10001000);        
   Wire.endTransmission();
-  delay(100);                    // Wait for reset to complete.
+  delay(100);                 
 
-  Wire.beginTransmission(0x68);  // Device address.
-  Wire.write(0x68);              // SIGNAL_PATH_RESET register.
-  Wire.write(0b00000111);        // GYRO_RESET, ACCEL_RESET, TEMP_RESET.
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x68);            
+  Wire.write(0b00000111);       
   Wire.endTransmission();
-  delay(100);                    // Wait for reset to complete.
+  delay(100);                  
 
-  // Disable SLEEP mode because the reset re-enables it. Section 3, PWR_MGMT_1 register, page 8.
-  Wire.beginTransmission(MPUAddress);   // Device address.
-  Wire.write(0x6B);                     // PWR_MGMT_1 register.
-  Wire.write(0b00001000);               // SLEEP = 0, TEMP_DIS = 1.
+  Wire.beginTransmission(MPUAddress);  
+  Wire.write(0x6B);                     
+  Wire.write(0b00001000);               
   Wire.endTransmission();
 }
 void setup() {
@@ -98,7 +92,7 @@ void loop() {
   Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometerX));
   Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometerY));
   Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometerZ));
-   //the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+
   Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
   Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyroX));
   Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyroY));
@@ -132,36 +126,35 @@ void loop() {
 //   switch (state)
 //   {
 //     case State::Request:
-//       // Request data from gyro data registers.
-//       Serial.print(F("Requesting data from gyro..."));
-//       Wire.beginTransmission(MPUAddress);   // Device address.
-//       Wire.write(0x43);                     // GYRO_XOUT_H register.
-//       Wire.endTransmission(false);          // Restart connection, i.e. keep alive.
-//       Wire.requestFrom(MPUAddress, 6);      // GYRO_XOUT_H, GYRO_XOUT_L, GYRO_YOUT_H, GYRO_YOUT_L, GYRO_ZOUT_H, GYRO_ZOUT_L.
+
+//       Wire.beginTransmission(MPUAddress);   
+//       Wire.write(0x43);                   
+//       Wire.endTransmission(false);          
+//       Wire.requestFrom(MPUAddress, 6);      
 //       Serial.println(F(" Done."));
 //       state = State::Wait;
 //       Serial.print(F("Waiting for data from gyro..."));
 //       break;
 
 //     case State::Wait:
-//       // Wait for data to arrive into buffer.
+
 //       if(Wire.available() < 6)
 //       {
 //         //Serial.print(F("."));
 //         Serial.print(F(" "));
-//         Serial.print(Wire.available());  // Added to show bytes received.
+//         Serial.print(Wire.available());  
 //       }
 //       else
 //       {
 //         Serial.print(F(" "));
-//         Serial.print(Wire.available());  // Added to show bytes received.
+//         Serial.print(Wire.available());  
 //         Serial.println(F(" Done."));
 //         state = State::Read;
 //       }
 //       break;
 
 //     case State::Read:
-//       // Read data from gyro registers.
+//
 //       Serial.print(F("Reading gyro data..."));
 //       gyroX = Wire.read() << 8 | Wire.read();
 //       gyroY = Wire.read() << 8 | Wire.read();
